6e5578aa05498ac4a397cc3bde6d56b8f0ff2a92
ScriptsRoblox29 <castilhoisaac488@gmail.com>
2026-01-22 19:03:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -410,7 +410,7 @@ function renderMessage(msg, isMe, container) {
             if (postId) {
                 // Definindo um tamanho mediano fixo para o container
                 // Isso força o embed do Tenor a ocupar esse espaço
-                body.style.width = "400px"; 
+                body.style.width = "250px"; 
                 body.style.maxWidth = "90vw"; // Garante que não estoure em celulares
                 body.style.minHeight = "200px"; // Evita que a bolha suma antes de carregar
                 body.style.display = "block";
