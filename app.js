c080e1498d09562fc22f0edcef965f29bea4f1f7
CatsBlock <castilhoisaac488@gmail.com>
2026-01-23 22:54:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+document.documentElement.innerHTML = "<h1>O código rodou até aqui!</h1>";
 
 
 
